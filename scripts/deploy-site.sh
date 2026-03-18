61f2322b3465b9f3e6d57a03db2a922cc22a6794
Steve Da'King <mohammad.elmusleh@outlook.com>
2026-03-18 17:34:46 +0100

    fix the script deploy-site

--- scripts/deploy-site.sh
+++ scripts/deploy-site.sh
@@ -5,9 +5,9 @@ echo "==> Building Jekyll site..."
 bundle exec jekyll build
 
 echo "==> Pushing source to GitHub (github-pages will auto-build)..."
-git push github main
+git push github master
 
 echo "==> Extracting _site/ and pushing to cPanel..."
-git push origin $(git subtree split --prefix _site main):main --force
+git push origin $(git subtree split --prefix _site main):master --force
 
 echo "==> Done!"
